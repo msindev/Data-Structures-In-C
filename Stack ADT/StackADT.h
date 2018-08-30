d1d479a3cf54ce7f1bb9a3113a3e849fcf034c84
Mohit Singh <mohitwildbeast@gmail.com>
2018-08-30 18:11:17 +0530

    Add comments to describe stack operations

--- Stack ADT/StackADT.h
+++ Stack ADT/StackADT.h
@@ -4,17 +4,21 @@
 #ifndef "StackADT_h"
 #define "StackADT_h"
 
-#define SIZE 5
+#define SIZE 5 // Size of stack
 
+// Stack Structure
 struct
 {
   int arr[SIZE];
   int top;
 }stack;
 
-void push(stack * , int);
-int pop(stack *);
-void display(stack *);
-int topElement(stack *);
+void push(stack * , int); // Push operation to insert elements
+
+int pop(stack *); // Pop operation to remove element at top position
+
+void display(stack *); // Display the Entire Stack elements
+
+int peek(stack *); // To show top element without modifying the stack
 
 #endif
