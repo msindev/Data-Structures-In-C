9c3e2992f1b73b1407e392357d27592622e076e4
Mohit Singh <mohitwildbeast@gmail.com>
2018-09-02 21:37:27 +0530

    Add header file and function prototypes for Circular Queue ADT.

---
+++ Circular Queue ADT/CircularQueueADT.h
@@ -0,0 +1,23 @@
+#ifndef CircularQueueADT_h
+#define CircularQueueADT_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 5
+typedef struct
+{
+  int front;
+  int rear;
+  int arr[SIZE];
+}queue;
+
+int getFront(queue *);
+int getRear(queue *);
+void enqueue(queue *, int);
+int dequeue(queue *);
+void display(queue *);
+int isFull(queue *);
+int isEmpty(queue *);
+
+#endif
