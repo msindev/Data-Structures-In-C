d8d2ce871c880c89804abf0fc34773052ce3f219
Mohit Singh <mohitwildbeast@gmail.com>
2018-09-25 01:27:30 +0530

    Revert to old code

--- Circular Queue ADT/CircularQueueADT.h
+++ Circular Queue ADT/CircularQueueADT.h
@@ -14,21 +14,3 @@ char* dequeue(queue *);
 void display(queue *);
 int isFull(queue *);
 int isEmpty(queue *);
-
-int isFull(queue *q)
-{
-  if((q->rear == SIZE - 1 && q->front == 0) || (q->rear == q->front - 1))
-  {
-    return 1;
-  }
-  return 0;
-}
-
-int isEmpty(queue *q)
-{
-  if(q->front == -1)
-  {
-    return 1;
-  }
-  return 0;
-}
