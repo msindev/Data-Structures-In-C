68fad83fbcf0d6ffdb5f43e7db308ac6a023e0c3
Mohit Singh <mohitwildbeast@gmail.com>
2018-09-25 01:17:33 +0530

    Merge code to Github

--- Circular Queue ADT/CircularQueueADT.h
+++ Circular Queue ADT/CircularQueueADT.h
@@ -1,6 +1,3 @@
-#ifndef CircularQueueADT_h
-#define CircularQueueADT_h
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,13 +6,31 @@ typedef struct
 {
   int front;
   int rear;
-  int arr[SIZE];
+  char arr[SIZE][100];
 }queue;
 
-void enqueue(queue *, int);
-int dequeue(queue *);
+void enqueue(queue *, char[100]);
+char* dequeue(queue *);
 void display(queue *);
 int isFull(queue *);
 int isEmpty(queue *);
 
-#endif
+int isFull(queue *q)
+{
+  if((q->rear == SIZE - 1 && q->front == 0) || (q->rear == q->front - 1))
+  {
+    return 1;
+  }
+  return 0;
+}
+
+int isEmpty(queue *q)
+{
+  if(q->front == -1)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+
