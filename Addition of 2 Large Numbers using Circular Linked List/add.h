852563044fa320656bf1251fce39b01f1a312fb1
Mohit Singh <mohitwildbeast@gmail.com>
2018-10-08 16:01:30 +0530

    Add header file for Addition of 2 large numbers using CLL

---
+++ Addition of 2 Large Numbers using Circular Linked List/add.h
@@ -0,0 +1,22 @@
+#ifndef add_h
+#define add_h
+
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+  struct node *next;
+};
+
+struct node *getnode()
+{
+  struct node *newnode = (struct node *)malloc(sizeof(struct node));
+  newnode -> next = newnode;
+  return newnode;
+}
+
+struct node *insert(struct node *,int);
+struct node *addToList(struct node *, struct node *);
+void printlist(struct node *);
+
+#endif
